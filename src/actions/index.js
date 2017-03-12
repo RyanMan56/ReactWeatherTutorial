1863c655c01564fd2420f7d7ea934c5faf54123f
Ryan Jones <Ryan Jones>
2017-03-12 15:18:34 +0000

    Finished tutorial

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '04a240b80754d292668dae48c6bc7d82';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
---
+++ src/containers/weather_list.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const { name, coord } = cityData.city;
+    const { lon, lat } = coord;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    console.log(temps);
+    return (
+      <tr key={name}>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
+        <td><Chart data={temps} units='°C' color={'orange'} /></td>
+        <td><Chart data={pressures} units='hPa' color={'green'} /></td>
+        <td><Chart data={humidities} units='%' color={'black'} /></td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (°C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
